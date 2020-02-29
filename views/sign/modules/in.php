9e6b298b14a387513cb5f428e7ef678f3cbab976
Vladislav <vladislavhacker111@gmail.com>
2020-02-29 15:17:23 +0300

    engine

---
+++ views/sign/modules/in.php
@@ -0,0 +1,5 @@
+<form enctype="multipart/form-data" action="./?cmd=sign&act=in" method="post">
+  <input type="email" name="email" placeholder="email">
+  <input type="password" name="password" placeholder="password">
+  <input type="submit">
+</form>
