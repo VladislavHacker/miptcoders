e135076874a56e40563760b65c5e495fd3b3dcf0
Vladislav <vladislavhacker111@gmail.com>
2020-03-01 14:57:06 +0300

    ...

--- profile/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $CONFIG['dbuser'] = 'root';
-    $CONFIG['dbpass'] = 'root';
-    $CONFIG['dbhost'] = 'localhost';
-    $CONFIG['dbname'] = 'miptcoders';
-
-    $CONFIG['mode'] = 'dev'; //dev or prod
-    $CONFIG['host'] = 'mipt';
-?>
--- profile/template/footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-  Eduq @ 2020
-</footer>
